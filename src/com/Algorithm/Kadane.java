2eae2a8b51615c4b3219a2b4234a5dbf15ff7513
Jkang1221 <88623655+Jkang1221@users.noreply.github.com>
2021-11-20 00:11:50 -0500

    Update Kadane.java

--- src/com/Algorithm/Kadane.java
+++ src/com/Algorithm/Kadane.java
@@ -3,7 +3,7 @@ package com.Algorithm;
 public class Kadane {
     public static void main(String[] args) {
         int[] nums = new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4};
-        System.out.println(kadane(nums));
+        System.out.println(kadane(nums.    ));
     }
 
     public static int kadane(int[] nums) {
